e89db0a475321a79251cfecb42b10d5810232fa2
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-12-31 09:11:13 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { findAllPackage, removePackage, updatePackage } = require("./database/db-func");
+
+updatePackage('A', 'B', 'C1', 'D11').then(x => console.log(x))
+    .then(() => {
+        findAllPackage()
+            .then(documents => {
+                console.log(documents)
+            })
+            .then(() => {
+                updatePackage('A1', 'B', 'C1', 'D1').then(x => console.log(x))
+                    .then(() => {
+                        removePackage('A1', 'B', 'C1', 'D1').then(x => console.log(x))
+                    })
+            })
+    })
+
+// async function printInput() {
+//     const streamReadAll = require('stream-read-all')
+//     console.log('reading');
+//     const stdin = await streamReadAll(process.stdin)
+//     console.log(stdin.toString())
+// }
+// printInput()
